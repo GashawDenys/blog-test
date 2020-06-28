3415bb8d8e71d370f24194e6bb35d5ec072ccc60
denysgashaw <denys.gashaw@gmail.com>
2020-06-29 02:40:00 +0300

    Added pagination on the main page

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -34,8 +34,8 @@ class Post(models.Model):
     class Meta:
         ordering = ('-publish',)
 
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.day, self.publish.month, self.publish.year, self.slug])
-
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
