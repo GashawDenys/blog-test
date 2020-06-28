042a048bc9bdbb25f8fb6193da01af951159e4f8
denysgashaw <denys.gashaw@gmail.com>
2020-06-29 01:23:09 +0300

    Added post templates and styles

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -9,16 +15,27 @@ class Post(models.Model):
         ('published', 'Published')
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(
+        max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(
+        default=timezone.now)
+    created = models.DateTimeField(
+        auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.day, self.publish.month, self.publish.year, self.slug])
 
     def __str__(self):
         return self.title
