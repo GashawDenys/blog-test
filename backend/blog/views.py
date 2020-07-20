f124295d8bfa8b4abda4f3509f0d54ae79af77b8
denysgashaw <denys.gashaw@gmail.com>
2020-07-20 03:45:55 +0300

    Added search form

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 from taggit.models import Tag
 
@@ -84,3 +85,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(
+                similarity=TrigramSimilarity('title', query)
+            ).filter(similarity__gt=0.3).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
